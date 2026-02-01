58b92758dd4db501cbd7514ef753909abc1c2946
Francois-Coleongco <christien.coleongco@gmail.com>
2026-01-31 23:28:39 -0500

    checkpointing

--- common/data.cc
+++ common/data.cc
@@ -2,11 +2,9 @@
 #include <cmath>
 #include <cstddef>
 #include <cstdint>
-#include <map>
 #include <memory>
 #include <queue>
 #include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 using u32 = uint32_t;
@@ -24,13 +22,18 @@ enum class PacketType : uint8_t {
   CLOSE
 };
 
-struct Header {
+class Header {
   PacketType type;
   u32 conn_id;
   u64 seq;
   u64 ack;
   u64 ack_bits;
   u64 timestamp_ns;
+
+  Header(PacketType type, u32 conn_id, u64 seq, u64 ack, u64 ack_bits,
+         u64 timestamp_ns)
+      : type(type), conn_id(conn_id), seq(seq), ack(ack),
+        timestamp_ns(timestamp_ns) {}
 };
 
 class Packet {
@@ -38,32 +41,48 @@ public:
   Header header;
   std::vector<u8> payload;
   size_t size() const { return sizeof(Header) + payload.size(); }
-  Packet() {};
+  Packet(const std::vector<u8> &data, Header header) : header(header) {
+    payload = std::move(data);
+  };
 };
 
 class Connection {
-  u64 seq_to_send;
-  u16 longest_contiguous_sequence;
-  std::chrono::nanoseconds rtt_smoothed;
-  std::chrono::nanoseconds rtt_variance;
-  u64 congestion_window;
-  u64 slow_start_threshold;
-  u64 inflight_bytes;
-  std::unordered_map<u64, std::shared_ptr<Packet>> inflight_tracker;
-  std::queue<std::shared_ptr<Packet>> send_queue;
+  u32 _conn_id;
+  u64 _seq_to_send;
+  u16 _longest_contiguous_sequence;
+  std::chrono::nanoseconds _rtt_smoothed;
+  std::chrono::nanoseconds _rtt_variance;
+  u64 _congestion_window;
+  u64 _slow_start_threshold;
+  u64 _inflight_bytes;
+  std::unordered_map<u64, std::shared_ptr<Packet>> _inflight_tracker;
+  std::queue<std::shared_ptr<Packet>> _send_queue;
 
   Connection()
-      : seq_to_send(0), longest_contiguous_sequence(0),
-        rtt_smoothed(std::chrono::nanoseconds(0)),
-        rtt_variance(std::chrono::nanoseconds(0)), congestion_window(12000),
-        slow_start_threshold() {};
+      : _seq_to_send(0), _longest_contiguous_sequence(0),
+        _rtt_smoothed(std::chrono::nanoseconds(0)),
+        _rtt_variance(std::chrono::nanoseconds(0)), _congestion_window(12000),
+        _slow_start_threshold(INFINITY), _inflight_bytes(0) {};
 
 public:
-  static Connection &init_handshake() {
+  static Connection &init_handshake(Header init_header) {
     static Connection c;
+    // Talk to server, ask for conn_id
+    std::vector<u8> config_info = {0}; // Future encryption things will be here I think, and maybe other config info I might need
+    Packet init_packet = Packet(config_info, init_header);
+    // Read conn_id from server, set it in this-> and send with every subsequent
+    // Packet
     return c;
-    // talk to server, ask for conn_id
-    // read conn_id from server and send with every subsequent packet
+  }
+
+  void create_and_queue(const std::vector<u8> &data) {
+
+
+	Packet(data, Header(PacketType::DATA, _conn_id, _seq_to_send, a_ack));
+
+    // Create Packet and add to queue
+    // Header(PacketType type, u32 conn_id, u64 seq, u64 ack, u64 ack_bits,
+    //        u64 timestamp_ns)
   }
 };
 
